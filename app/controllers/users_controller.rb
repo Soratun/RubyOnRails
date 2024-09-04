c06ac2fe272a54162fef483615c5eba5c68575bd
Soratun <tunzax014@gmail.com>
2024-09-04 21:28:19 +0700

    add destroy in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,12 @@ class UsersController < ApplicationController
             render :edit
         end
     end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to root_path
+    end
     private
     def user_params
         params.require(:user).permit(:firstname, :lastname, :birthday, :gender, :email, :phonenumber, :subject)
