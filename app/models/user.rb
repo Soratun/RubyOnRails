9f9e3a9a002f67fec8f373ddaa7d82cc15f3cccf
Soratun <tunzax014@gmail.com>
2024-09-07 21:53:14 +0700

    update model add if validates to email and phonenumber

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
     validates :lastname, presence: { message: "Last Name can't be blank." }
     validates :birthday, presence: { message: "Birth Date can't be blank." }
     validates :gender, presence: { message: "Please select a gender." }
-    validates :email, presence: { message: "Email can't be blank." }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email format." }
-    validates :phonenumber, presence: { message: "Phone Number can't be blank." }, format: { with: /\A[0-9]+\z/, message: "Phone Number should contain only digits." }
+    validates :email, presence: { message: "Email can't be blank." }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email format." } ,if: -> { email.present? }
+    validates :phonenumber, presence: { message: "Phone Number can't be blank." }
+    validates :phonenumber, format: { with: /\A[0-9]+\z/, message: "Phone Number should contain only digits." } ,if: -> { phonenumber.present? }
     validates :subject, presence: { message: "Please select a subject." }
 end
